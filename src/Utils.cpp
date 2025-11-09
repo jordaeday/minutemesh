65577ed278f69c3b814371a918f70415a2e4fa28
leon332157 <leon332157@gmail.com>
2025-11-08 22:32:43 -0500

    get aes working?

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,100 +1,97 @@
 #include "Utils.h"
-#include <CTR.h>
+
 #include <AES.h>
+#include <CTR.h>
 #include <Crypto.h>
 #include <SHA256.h>
 
 #ifdef ARDUINO
-  #include <Arduino.h>
+#include <Arduino.h>
 #endif
 
 namespace mesh {
 
 uint32_t RNG::nextInt(uint32_t _min, uint32_t _max) {
   uint32_t num;
-  random((uint8_t *) &num, sizeof(num));
+  random((uint8_t *)&num, sizeof(num));
   return (num % (_max - _min)) + _min;
 }
 
-void Utils::sha256(uint8_t *hash, size_t hash_len, const uint8_t* msg, int msg_len) {
+void Utils::sha256(uint8_t *hash, size_t hash_len, const uint8_t *msg, int msg_len) {
   SHA256 sha;
   sha.update(msg, msg_len);
   sha.finalize(hash, hash_len);
 }
 
-void Utils::sha256(uint8_t *hash, size_t hash_len, const uint8_t* frag1, int frag1_len, const uint8_t* frag2, int frag2_len) {
+void Utils::sha256(uint8_t *hash, size_t hash_len, const uint8_t *frag1, int frag1_len, const uint8_t *frag2,
+                   int frag2_len) {
   SHA256 sha;
   sha.update(frag1, frag1_len);
   sha.update(frag2, frag2_len);
   sha.finalize(hash, hash_len);
 }
 
-int Utils::decrypt(const uint8_t* shared_secret, uint8_t* dest, const uint8_t* src, int src_len) {
+int Utils::decrypt(const uint8_t *shared_secret, uint8_t *dest, const uint8_t *src, int src_len) {
   AES128 aes;
-  uint8_t* dp = dest;
-  const uint8_t* sp = src;
+  uint8_t *dp = dest;
+  const uint8_t *sp = src;
 
   aes.setKey(shared_secret, CIPHER_KEY_SIZE);
   while (sp - src < src_len) {
     aes.decryptBlock(dp, sp);
-    dp += 16; sp += 16;
+    dp += 16;
+    sp += 16;
   }
 
-  return sp - src;  // will always be multiple of 16
+  return sp - src; // will always be multiple of 16
 }
 
-void Utils::decryptAESCtr(const uint32_t fromNode, uint64_t packetId, size_t numBytes, uint8_t *bytes)
-{
-  uint8_t nonce[16] = {0};
+void Utils::decryptAESCtr(const uint32_t fromNode, uint64_t packetId, size_t numBytes, uint8_t *bytes) {
+  uint8_t nonce[16] = { 0 };
   // For CTR, the implementation is the same
   // TODO extract the nonce using packetId
+  memcpy(nonce, &packetId, sizeof(uint64_t));
+  memcpy(nonce + sizeof(uint64_t), &fromNode, sizeof(uint32_t));
   encryptAESCtr(nonce, numBytes, bytes);
 }
 
-int Utils::encrypt(const uint8_t* shared_secret, uint8_t* dest, const uint8_t* src, int src_len) {
+int Utils::encrypt(const uint8_t *shared_secret, uint8_t *dest, const uint8_t *src, int src_len) {
   AES128 aes;
-  uint8_t* dp = dest;
+  uint8_t *dp = dest;
 
   aes.setKey(shared_secret, CIPHER_KEY_SIZE);
   while (src_len >= 16) {
     aes.encryptBlock(dp, src);
-    dp += 16; src += 16; src_len -= 16;
+    dp += 16;
+    src += 16;
+    src_len -= 16;
   }
-  if (src_len > 0) {  // remaining partial block
+  if (src_len > 0) { // remaining partial block
     uint8_t tmp[16];
     memset(tmp, 0, 16);
     memcpy(tmp, src, src_len);
     aes.encryptBlock(dp, tmp);
     dp += 16;
   }
-  return dp - dest;  // will always be multiple of 16
+  return dp - dest; // will always be multiple of 16
 }
 
-void Utils::encryptAESCtr(uint8_t *_nonce, size_t numBytes, uint8_t *bytes)
-{
-  uint8_t key;
-  CTRCommon ctr;
-
-  // TODO extract the nonce
-  // TODO set the correct key
-  key = 0x0;
-  ctr = nullptr;
-  if (_key.length == 16)
-    ctr = new CTR<AES128>();
-  else
-    ctr = new CTR<AES256>();
-  ctr->setKey(key, key.length);
-  static uint8_t scratch[MAX_BLOCKSIZE];
+void Utils::encryptAESCtr(uint8_t *_nonce, size_t numBytes, uint8_t *bytes) {
+  CTR<AES128> ctr;
+  uint8_t key[16] = { 0xd4, 0xf1, 0xbb, 0x3a, 0x20, 0x29, 0x07, 0x59,
+                      0xf0, 0xbc, 0xff, 0xab, 0xcf, 0x4e, 0x69, 0xe5 };
+  ctr.setKey(key, sizeof(key));
+  static uint8_t scratch[256];
   memcpy(scratch, bytes, numBytes);
   memset(scratch + numBytes, 0,
          sizeof(scratch) - numBytes); // Fill rest of buffer with zero (in case cypher looks at it)
 
-  ctr->setIV(_nonce, 16);
-  ctr->setCounterSize(4);
-  ctr->encrypt(bytes, scratch, numBytes);
+  ctr.setIV(_nonce, 16);
+  ctr.setCounterSize(4);
+  ctr.encrypt(bytes, scratch, numBytes);
 }
 
-int Utils::encryptThenMAC(const uint8_t* shared_secret, uint8_t* dest, const uint8_t* src, int src_len) {
+int Utils::encryptThenMAC(const uint8_t *shared_secret, uint8_t *dest, const uint8_t *src, int src_len) {
   int enc_len = encrypt(shared_secret, dest + CIPHER_MAC_SIZE, src, src_len);
 
   SHA256 sha;
@@ -105,8 +102,8 @@ int Utils::encryptThenMAC(const uint8_t* shared_secret, uint8_t* dest, const uin
   return CIPHER_MAC_SIZE + enc_len;
 }
 
-int Utils::MACThenDecrypt(const uint8_t* shared_secret, uint8_t* dest, const uint8_t* src, int src_len) {
-  if (src_len <= CIPHER_MAC_SIZE) return 0;  // invalid src bytes
+int Utils::MACThenDecrypt(const uint8_t *shared_secret, uint8_t *dest, const uint8_t *src, int src_len) {
+  if (src_len <= CIPHER_MAC_SIZE) return 0; // invalid src bytes
 
   uint8_t hmac[CIPHER_MAC_SIZE];
   {
@@ -123,7 +120,7 @@ int Utils::MACThenDecrypt(const uint8_t* shared_secret, uint8_t* dest, const uin
 
 static const char hex_chars[] = "0123456789ABCDEF";
 
-void Utils::toHex(char* dest, const uint8_t* src, size_t len) {
+void Utils::toHex(char *dest, const uint8_t *src, size_t len) {
   while (len > 0) {
     uint8_t b = *src++;
     *dest++ = hex_chars[b >> 4];
@@ -133,7 +130,7 @@ void Utils::toHex(char* dest, const uint8_t* src, size_t len) {
   *dest = 0;
 }
 
-void Utils::printHex(Stream& s, const uint8_t* src, size_t len) {
+void Utils::printHex(Stream &s, const uint8_t *src, size_t len) {
   while (len > 0) {
     uint8_t b = *src++;
     s.print(hex_chars[b >> 4]);
@@ -153,11 +150,11 @@ bool Utils::isHexChar(char c) {
   return c == '0' || hexVal(c) > 0;
 }
 
-bool Utils::fromHex(uint8_t* dest, int dest_size, const char *src_hex) {
+bool Utils::fromHex(uint8_t *dest, int dest_size, const char *src_hex) {
   int len = strlen(src_hex);
-  if (len != dest_size*2) return false;  // incorrect length
+  if (len != dest_size * 2) return false; // incorrect length
 
-  uint8_t* dp = dest;
+  uint8_t *dp = dest;
   while (dp - dest < dest_size) {
     char ch = *src_hex++;
     char cl = *src_hex++;
@@ -166,22 +163,24 @@ bool Utils::fromHex(uint8_t* dest, int dest_size, const char *src_hex) {
   return true;
 }
 
-int Utils::parseTextParts(char* text, const char* parts[], int max_num, char separator) {
+int Utils::parseTextParts(char *text, const char *parts[], int max_num, char separator) {
   int num = 0;
-  char* sp = text;
+  char *sp = text;
   while (*sp && num < max_num) {
     parts[num++] = sp;
-    while (*sp && *sp != separator) sp++;
+    while (*sp && *sp != separator)
+      sp++;
     if (*sp) {
-       *sp++ = 0;  // replace the seperator with a null, and skip past it
+      *sp++ = 0; // replace the seperator with a null, and skip past it
     }
   }
   // if we hit the maximum parts, make sure LAST entry does NOT have separator
-  while (*sp && *sp != separator) sp++;
+  while (*sp && *sp != separator)
+    sp++;
   if (*sp) {
-    *sp = 0;  // replace the separator with null
+    *sp = 0; // replace the separator with null
   }
   return num;
 }
 
-}
+} // namespace mesh
